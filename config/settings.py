ccc6970e1c21c7eb2b41b48ca3dbf16e5523ebc8
Zachary <zacharyjhunn@gmail.com>
2020-11-09 01:27:11 -0700

    redoing repo

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o2(phb-5f4-0(q=v2%_k=gvlr669+@d*xu3m6*t^worow9d-6t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['herokuapp.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
